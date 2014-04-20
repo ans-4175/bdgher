9ebb0c7ed24934b199266f8c9a79013c1b6a8435
Ahmad Anshorimuslim Syuhada <ans4175@ans4175-PC>
2014-04-21 00:04:13 +0700

    beres tampilan dan data

--- asset/controller/stat.php
+++ asset/controller/stat.php
@@ -1,4 +1,19 @@
 <?php
 if (!defined('ROOT_PATH')) die('No direct access.');
 //view, if isset param, switch case, error view404
-
+if (true){
+	
+}else{
+	view_header_top();
+	echo "<title>Dashboard #BDGHER</title>";
+	view_header_bottom();
+	?>
+	<?php
+	view_footer_top();
+	?>
+	<script>
+		
+	</script>
+	<?php
+	view_footer_bottom();
+}
